fcb932e2e658f50b790d684dc79a49a65bf6a008
RV <vanyaraspopov@mail.ru>
2018-02-17 13:38:43 +0500

    Logger added

--- binance-bot.js
+++ binance-bot.js
@@ -1,8 +1,14 @@
 var binance = require('node-binance-api'),
-    config = require('./config/config');
+    config = require('./config/config'),
+    logger = require('./components/logger/logger');
 
 binance.options(config.get('binance'));
-binance.bookTickers((error, ticker) => {
-    console.log("bookTickers()", ticker);
-    console.log("Price of BNB: ", ticker.BNBBTC);
+binance.websockets.chart("BNBBTC", "1m", (symbol, interval, chart) => {
+    let tick = binance.last(chart);
+    const last = chart[tick].close;
+    //console.log(chart);
+    // Optionally convert 'chart' object to array:
+    // let ohlc = binance.ohlc(chart);
+    // console.log(symbol, ohlc);
+    logger.info(symbol+" last price: "+last);
 });
---
+++ components/logger/logger.js
@@ -0,0 +1,17 @@
+var winston = require('winston');
+
+var  logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.simple(),
+    transports: [
+        //
+        // - Write to all logs with level `info` and below to `combined.log`
+        // - Write all logs error (and below) to `error.log`.
+        //
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' })
+    ]
+});
+
+module.exports = logger;
