f792f699144e074c917aa0875a27b5b93ad212df
RV <vanyaraspopov@mail.ru>
2018-02-21 22:32:36 +0500

    Reading docs more accurately lead to more correct and clean code.

--- components/bot/bot.js
+++ components/bot/bot.js
@@ -9,7 +9,7 @@ let bot = {
     async run() {
         const delay = ms => new Promise(res => setTimeout(res, ms));
         while (true) {
-            let runExpect = 10 * 1000;  //  ms
+            let runExpect = 60 * 1000;  //  ms
             let runStart = moment();
 
             this.work();
@@ -33,15 +33,23 @@ let bot = {
         let prevMinuteStart_msts = prevMinuteStart.unix() * 1000;
         let prevMinuteEnd_msts = prevMinuteEnd.unix() * 1000;
 
-        this.getAggTrades(limit, prevMinuteStart_msts, prevMinuteEnd_msts, (response) => {
-            console.log("final response:", response.length);
+        this.getAggTrades(prevMinuteStart_msts, prevMinuteEnd_msts, (response) => {
+            let prices = [];
+            let quantity = 0;
+            for (let trade of response) {
+                prices.push(Number(trade.p));
+                quantity += Number(trade.q);
+            }
+            let avgPrice = prices.reduce((a, b) => a + b) / prices.length;
+            console.log("response length:", response.length);
+            console.log("avg price:", avgPrice);
+            console.log("total quantity:", quantity);
         });
     },
 
-    getAggTrades(limit, startTime, endTime, cb) {
+    getAggTrades(startTime, endTime, cb) {
         binance.aggTrades("ETHBTC",
             {
-                limit: limit,
                 startTime: startTime,
                 endTime: endTime
             },
@@ -50,18 +58,7 @@ let bot = {
                     console.log(error);
                     cb([]);
                 }
-                if (response.length == limit) {
-                    let middleTime = math.floor((startTime + endTime) / 2);
-                    let firstHalfEnd = middleTime;
-                    let secondHalfStart = middleTime + 1;
-                    this.getAggTrades(limit, startTime, firstHalfEnd, (res1) => {
-                        this.getAggTrades(limit, secondHalfStart, endTime, (res2) => {
-                            cb(res1.concat(res2));
-                        })
-                    });
-                } else {
-                    cb(response);
-                }
+                cb(response);
             });
     }
 };
