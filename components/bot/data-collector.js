3cf0b3b519031bd05ad79a380aeae6c96dd40774
RV <vanyaraspopov@mail.ru>
2018-03-11 14:04:39 +0500

    Getting symbols from database

--- components/bot/data-collector.js
+++ components/bot/data-collector.js
@@ -5,9 +5,9 @@ const moment = require('moment');
 //  Models
 const AggTrade = db.sequelize.models['AggTrade'];
 const Candle = db.sequelize.models['Candle'];
+const Currency = db.sequelize.models['Currency'];
 
 let config = {
-    symbols: ['ETHBTC', 'NEOBTC'],
     quantityPrecision: 8,
 };
 
@@ -16,6 +16,8 @@ class DataCollector {
     constructor(bb) {
         this.bb = bb;
         this.config = config;
+
+        this._symbols = undefined;
     }
 
     async getAggTrades(symbol, startTime, endTime) {
@@ -30,6 +32,25 @@ class DataCollector {
             this.bb.log.error(error);
         }
         return aggTrades;
+    }
+
+    async getSymbols() {
+        if (this._symbols !== undefined) {
+            return this._symbols;
+        }
+
+        let symbols = [];
+        try {
+            let currencies = await Currency.findAll();
+            for (let currency of currencies) {
+                symbols.push(currency.quot + currency.base);
+            }
+        } catch (error) {
+            this.bb.log.error(error);
+        }
+
+        this._symbols = symbols;
+        return this._symbols;
     }
 
     async collectCandles(gettingCandlesInterval) {
@@ -47,7 +68,7 @@ class DataCollector {
             });
         }
 
-        for (let symbol of this.config.symbols) {
+        for (let symbol of await this.getSymbols()) {
             try {
                 let candles = await this.bb.api.candles({
                     symbol: symbol,
@@ -87,7 +108,7 @@ class DataCollector {
         let prevMinuteStart_msts = prevMinuteStart.unix() * 1000;
         let prevMinuteEnd_msts = prevMinuteEnd.unix() * 1000 - 1;
 
-        for (let symbol of this.config.symbols) {
+        for (let symbol of await this.getSymbols()) {
             try {
                 let count = await AggTrade.count({
                     where: {
