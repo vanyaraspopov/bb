9e01e8e375423aaf48454b3f3cff129322ff389f
RV <vanyaraspopov@mail.ru>
2018-04-24 12:47:29 +0500

    Symbol filters

--- database/models/symbol.js
+++ database/models/symbol.js
@@ -28,20 +28,92 @@ module.exports = (sequelize, DataTypes) => {
         });
     };
 
-    let paramsToArray = function (results) {
-        let symbols = [];
-        if (!(results instanceof Array)) {
-            symbols.push(results)
+    /**
+     * Convert properties defined as JSON strings to objects
+     * @param {Array<Symb>|Symb} symbols
+     */
+    Symb.paramsToArray = function (symbols) {
+        let _symbols = [];
+        if (!(symbols instanceof Array)) {
+            _symbols.push(symbols)
         } else {
-            symbols = results;
+            _symbols = symbols;
         }
-        for (let symbol of symbols) {
+        for (let symbol of _symbols) {
             symbol.orderTypes = JSON.parse(symbol.orderTypes);
             symbol.filters = JSON.parse(symbol.filters);
         }
     };
 
-    Symb.hook('afterFind', paramsToArray);
+    /**
+     *
+     * @param key
+     * @returns {*}
+     */
+    Symb.prototype.getFilter = function (key) {
+        if (this.filters instanceof String) {
+            Symb.paramsToArray(this);
+        }
+        for (let filter of this.filters) {
+            if (filter['filterType'] === key) {
+                return filter;
+            }
+        }
+    };
+
+    /**
+     * Check if order type accessible for this symbol
+     * @param orderType
+     * @returns {boolean}
+     */
+    Symb.prototype.checkOrderType = function (orderType) {
+        if (this.orderTypes instanceof String) {
+            Symb.paramsToArray(this);
+        }
+        return this.orderTypes.includes(orderType);
+    };
+
+    Symb.prototype.checkPrice = function (price) {
+        let filter = this.getFilter('PRICE_FILTER');
+        let min = Number(filter['minPrice']);
+        let max = Number(filter['maxPrice']);
+        let step = Number(filter['tickSize']);
+        let isCorrect = true;
+        isCorrect &= (price >= min);
+        isCorrect &= (price <= max);
+        isCorrect &= (price % step === 0);
+        return isCorrect;
+    };
+    Symb.prototype.checkLotSize = function (quantity) {
+        let filter = this.getFilter('LOT_SIZE');
+        let min = Number(filter['minQty']);
+        let max = Number(filter['maxQty']);
+        let step = Number(filter['stepSize']);
+        let isCorrect = true;
+        isCorrect &= (quantity >= min);
+        isCorrect &= (quantity <= max);
+        isCorrect &= (quantity % step === 0);
+        return isCorrect;
+    };
+    Symb.prototype.checkMinNotional = function (price, quantity) {
+        let filter = this.getFilter('MIN_NOTIONAL');
+        let notional = price * quantity;
+        let min = Number(filter['minNotional']);
+        return notional >= min;
+    };
+
+    /**
+     * Check if price and quantity pass symbol filters
+     * @param price
+     * @param quantity
+     */
+    Symb.prototype.checkFilters = function (price, quantity) {
+        this.checkPrice(price);
+        this.checkLotSize(quantity);
+        this.checkMinNotional(price, quantity);
+    };
+
+    Symb.hook('afterFind', Symb.paramsToArray);
 
     return Symb;
 };
